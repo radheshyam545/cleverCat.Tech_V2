befee096cdcac529ea834f85c5a0c8889b7e8d77
radheshyam545 <kumawatradhe545@gmail.com>
2025-09-03 13:26:50 +0530

    change api url on production

--- services/apiService.ts
+++ services/apiService.ts
@@ -5,6 +5,7 @@ import { AuditData } from "@/types/audit";
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://junoapp.clevercat.ai/api";
 // const API_BASE_URL = "http://localhost:5000/api";
 
+
 interface FormData {
   first_name: string;
   last_name: string;
